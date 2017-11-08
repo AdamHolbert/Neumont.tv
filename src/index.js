ca4a568f869cf3f4a4752223d213a45e8b8ec334
AdamHolbert <aholbert@student.neumont.edu>
2017-11-08 14:20:42 -0700

    auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 // 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
@@ -11,14 +13,26 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // 2
-const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9q3agfp6w5i0109a6ezpx6d' })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9ri3jbt4o890101g81tgwgg' })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    const authorizationHeader = token ? `Bearer ${token}` : null
+    operation.setContext({
+        headers: {
+            authorization: authorizationHeader
+        }
+    })
+    return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
 // 3
 const client = new ApolloClient({
-    link: httpLink,
+    link: httpLinkWithAuthToken,
     cache: new InMemoryCache()
 })
-
 // 4
 ReactDOM.render(
     <BrowserRouter>
