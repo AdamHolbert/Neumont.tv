f6359e123188a3995e25f6f188131b3774f8a8cb
AdamHolbert <aholbert@student.neumont.edu>
2017-11-21 14:38:27 -0700

    Added header info

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,41 @@ import Search from './Search'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants'
+import logoPicture from '../Logo.png'
 
 class Header extends Component {
-
-    render() {
-        const userId = localStorage.getItem(GC_USER_ID)
-        return (
-            <div>
-                <div>
-                    <div>Hacker News</div>
-                    <Link to='/'>new</Link>
-                    {userId &&
-                    <div>
-                        <div>|</div>
-                        <Link to='/create'>submit</Link>
-                    </div>
-                    }
-                </div>
-                <div>
-                    {userId ?
-                        <div onClick={() => {
-                            localStorage.removeItem(GC_USER_ID)
-                            localStorage.removeItem(GC_AUTH_TOKEN)
-                            this.props.history.push(`/`)
-                        }}>logout</div>
-                        :
-                        <Link to='/login'>login</Link>
-                    }
-                </div>
-                <Search />
-            </div>
-        )
-    }
+  
+  render() {
+    const userId = localStorage.getItem(GC_USER_ID)
+    return (
+      <div className='header'>
+        <img src={logoPicture} className='headerLogo'/>
+        <div className='headerBar'>
+          <div className='headerTop'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About us</Link>
+            <Link to='/contact'>Contact us</Link>
+            
+            {userId ?
+              <div onClick={() => {
+                localStorage.removeItem(GC_USER_ID)
+                localStorage.removeItem(GC_AUTH_TOKEN)
+                this.props.history.push(`/`)
+              }}>logout</div>
+              :
+              <Link to='/login'>login</Link>
+            }
+          </div>
+          <div className='headerBotton'>
+            <Search />
+            {!userId && <Link to='/create'>New User</Link> }
+          </div>
+          
+          
+        </div>
+      </div>
+    )
+  }
 
 }
 
