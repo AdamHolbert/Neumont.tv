23a702e067bc215cc732465221085d6c0f841787
Priya Vora <PVora@student.neumont.edu>
2017-11-29 15:40:17 -0700

    Have disclaimer.css in process

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { HIDE_DISCLAIMER } from '../constants';
-
+import '../styles/disclaimer.css'
 class Disclaimer extends Component {
   
   constructor(props){
@@ -15,17 +15,18 @@ class Disclaimer extends Component {
       <div className='disclaimer'>
         This website is not affiliated with, nor does anything said or posted on this website represent,
         the views of: Neumont College of Computer Science.
+          <br />
+          (Check the box at right: never see this message again.)
         
-        <input type="checkbox"
-               value={this.state.neverAgain}
-               onChange={(e) => {
-                 this.setState({
-                   neverAgain: e.target.checked
-                 });
-               }}
-        />
+            <div className={this.state.neverAgain ? 'fakeCheckBox checked' : 'fakeCheckBox unchecked'}
+                   onClick={(e) => {
+                     this.setState({
+                       neverAgain: !this.state.neverAgain
+                     });
+                   }}
+            />
         
-        <button onClick={() => {
+        <button className='hide' onClick={() => {
           console.log("Clicked: ", this.state.neverAgain)
           localStorage.setItem(HIDE_DISCLAIMER, this.state.neverAgain);
           this.props.hideDisclaimer();
