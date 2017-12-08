bd20a37a3dd923c9625bd444613ae9563ab7f7a2
Priya Vora <PVora@student.neumont.edu>
2017-12-08 09:19:36 -0700

    About Us Content Updated

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -14,7 +14,32 @@ class AboutUs extends Component {
                     <div className='ContactContainer'>
 
                         <div className='text'>
-                            Message about us...
+                            <p>
+                                Group Name: Neumont.tv
+                                Members:
+                                Priya Vora,
+                                <br />
+                                Adam Holbert,
+                                <br/>
+                                William Walsh
+                                <br />
+                            </p>
+                            Project Motive:
+                            <p className='projectMotive'>
+                            We wanted to to build an application that would be challenging and practical,
+                            something we would see in the real world. In simple terms, a website that is a basic imitation of Youtube.
+                            Our project is centered around us taking advantage of the fact that this course is language agnostic.
+                            As a group we decided upon on a video streaming application, due to none of us members knowing how to
+                            develop the hand shake between the server and client to stream and upload an entire video. For this group project
+                            we want to further our knowledge upon the basics of making a web application.
+                            This project is broken up into three parts.The main purpose of our project is to record live videos on to our website.
+                            Users would be able to upload various length of videos on our website,where the data would be persisted within Graph.Cool.
+                            We are utilizing React as a library to enhance the view of our web application.
+                            We made this decision to learn more about the industry standards and potential job opportunities.
+                            The purpose of our website is not to compete with existing companies, however to give the public another platform that's user friendly and convenient.This project is a means to take advantage of the time and teach ourselves how video streaming works.</p>
+
+                            <br />
+
                         </div>
 
                     </div>
