79f86728803e50363af6d5a02bcd44f97db23382
AdamHolbert <aholbert@student.neumont.edu>
2018-04-13 10:16:29 -0600

    Fixed math

--- src/components/Math.js
+++ src/components/Math.js
@@ -7,7 +7,7 @@ class Math extends Component {
     constructor(props) {
         super();
         this.state = {
-            matrix: [],
+            savedMatrices: [],
             twoXtwo: true,
             loading: false,
             output: null,
@@ -34,11 +34,12 @@ class Math extends Component {
         }
     }
     
-    
-    
     render() {
         return (
             <div className='uploadContainer'>
+                <div className='output' dangerouslySetInnerHTML={{ __html: oldMatrices(this.state.savedMatrices)}}>
+               
+                </div>
                 <div>
                     <div className="mathFlex">
                         <div onClick={(e) => this.setState({twoXtwo: true})} className={(this.state.twoXtwo ?
@@ -159,19 +160,51 @@ class Math extends Component {
                         </div>
                     </div>
                 }
-                <div className="button mathFlex"></div>
-                <div className='output'>
-                    {this.state.output}
+                <div className='button' onClick={() => {
+                    this.state.savedMatrices.push(getMatrixFromState(this.state));
+                    this.forceUpdate();
+                }}>
+                    Save Matrix
+                </div>
+                <div className='output' dangerouslySetInnerHTML={{ __html: this.state.output }}>
+                
                 </div>
             </div>
         )
     }
 }
+
+function getMatrixFromState(state) {
+    let matrix = '';
+    if (state.twoXtwo) {
+        matrix = [state.x2n1, state.x2n2, state.x2n3, state.x2n4];
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i] = parseInt(matrix[i]) || 0
+        }
+        matrix = [
+            [matrix[0], matrix[1]],
+            [matrix[2], matrix[3]]
+        ]
+    } else {
+        matrix = [state.x3n1, state.x3n2, state.x3n3, state.x3n4, state.x3n5, state.x3n6, state.x3n7, state.x3n8
+            , state.x3n9];
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i] = parseInt(matrix[i]) || 0
+        }
+        matrix = [
+            [matrix[0], matrix[1], matrix[2]],
+            [matrix[3], matrix[4], matrix[5]],
+            [matrix[6], matrix[7], matrix[8]]
+        ]
+    }
+    return matrix
+}
+
 function matrixMath(state) {
-    let display = '';
+    let display = '<div>';
     let matrix = '';
     if(state.twoXtwo){
-        matrix = [state.x2n1, state.x2n2, state.x2n3, state.x2n4]
+        matrix = [state.x2n1, state.x2n2, state.x2n3, state.x2n4];
         for (let i = 0; i < matrix.length; i++) {
             matrix[i] = parseInt(matrix[i]) || 0
         }
@@ -179,8 +212,6 @@ function matrixMath(state) {
             [matrix[0], matrix[1]],
             [matrix[2], matrix[3]]
         ]
-        
-        display += "Determinant: " + getDeterminant(matrix)
         
     } else {
         matrix = [state.x3n1, state.x3n2, state.x3n3, state.x3n4, state.x3n5, state.x3n6, state.x3n7, state.x3n8
@@ -193,21 +224,16 @@ function matrixMath(state) {
             [matrix[3], matrix[4], matrix[5]],
             [matrix[6], matrix[7], matrix[8]]
         ]
-    
-        display += "Determinant: " + getDeterminant(matrix)
     }
-    return display
+    display += "Determinant: " + getDeterminant(matrix)
+    display += "<br \>Inverse: " + matrixDisplay(getInverse(matrix))
+    return display + '</div>'
 }
 
 function getDeterminant(matrix){
     if(matrix[0].length === 2){
         return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
     } else if (matrix[0].length === 3) {
-        
-        console.log([
-            [matrix[1][1], matrix[1][2]],
-            [matrix[2][1], matrix[2][2]]
-        ]);
         return (
             (matrix[0][0]*getDeterminant([
                 [matrix[1][1], matrix[1][2]],
@@ -225,5 +251,90 @@ function getDeterminant(matrix){
     }
 }
 
+function getInverse(matrix){
+    let det = getDeterminant(matrix);
+    if(det !== 0){
+        det = 1/det
+    }
+    if(matrix.length === 2){
+        matrix = [
+            [matrix[1][1]*det, matrix[1][0]*-1*det],
+            [matrix[0][1]*-1*det, matrix[0][0]*det]
+        ];
+    } else if(matrix.length === 3){
+        matrix = [
+            [
+                getDeterminant([[matrix[1][1], matrix[1][2]],
+                                [matrix[2][1], matrix[2][2]]])*det,
+        
+                getDeterminant([[matrix[1][0], matrix[1][2]],
+                                [matrix[2][0], matrix[2][2]]])*det*-1,
+        
+                getDeterminant([[matrix[1][0], matrix[1][1]],
+                                [matrix[2][0], matrix[2][1]]])*det],
+            [
+                getDeterminant([[matrix[0][1], matrix[0][2]],
+                                [matrix[2][1], matrix[2][2]]])*det*-1,
+        
+                getDeterminant([[matrix[0][0], matrix[0][2]],
+                                [matrix[2][0], matrix[2][2]]])*det,
+        
+                getDeterminant([[matrix[0][0], matrix[0][1]],
+                                [matrix[2][0], matrix[2][1]]])*det*-1],
+            [
+                getDeterminant([[matrix[0][1], matrix[0][2]],
+                                [matrix[1][1], matrix[1][2]]])*det,
+        
+                getDeterminant([[matrix[0][0], matrix[0][2]],
+                                [matrix[1][0], matrix[1][2]]])*det*-1,
+        
+                getDeterminant([[matrix[0][0], matrix[0][1]],
+                                [matrix[1][0], matrix[1][1]]])*det],
+        ];
+    }
+    return transpose(matrix)
+}
+
+function transpose(matrix){
+    if(matrix.length === 2){
+        matrix = [
+            [matrix[0][0], matrix[1][0]],
+            [matrix[0][1], matrix[1][1]]
+        ]
+    } else if(matrix.length === 3){
+        matrix = [
+            [matrix[0][0], matrix[1][0], matrix[2][0]],
+            [matrix[0][1], matrix[1][1], matrix[2][1]],
+            [matrix[0][2], matrix[1][2], matrix[2][2]]
+        ]
+    }
+    return matrix
+}
+
+
+function oldMatrices(savedMatrices) {
+    let content = '<div class="multipleMatrixDisplay">';
+    for(let i = 0; i < savedMatrices.length; i++){
+        content += matrixDisplay(savedMatrices[i])
+    }
+    content +='</div>';
+    return content
+}
+
+
+function matrixDisplay(Matrix) {
+    let content = '<div class="matrixDisplay selectable">'
+    for(let x = 0; x < Matrix.length; x++){
+        content += '<div class=\'matrixRow\'>';
+        for(let y = 0; y < Matrix[x].length; y++){
+            content += '<div class=\'matrixColumn\'>';
+            content += Matrix[x][y];
+            content += '</div>';
+        }
+        content += '</div>';
+    }
+    content += '</div>';
+    return content;
+}
 
 export default Math
